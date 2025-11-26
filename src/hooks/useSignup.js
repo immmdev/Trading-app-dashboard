e3ed4ad5e8f710ae2d3fdca0409e5e1d72e4cc70
Dev Singh <154252296+immmdev@users.noreply.github.com>
2025-11-26 12:10:18 +0530

    Update useSignup.js

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 export const useSignup = () => {
     const [error, setError] = useState(null);
